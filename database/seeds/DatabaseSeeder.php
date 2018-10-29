ee0f690184872d6abedac37c66d3ed7a3f82e6a6
Marcelo Pereira <marcelooprogramador@gmail.com>
2018-10-29 15:18:05 -0300

    Made seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use \App\Models\Product;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call([
+            UsersTableSeeder::class,
+            ProductTableSeeder::class
+        ]);
     }
 }
---
+++ database/seeds/ProductTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use \App\Models\Product;
+
+class ProductTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+        factory(Product::class, 50)->create();
+
+    }
+}
